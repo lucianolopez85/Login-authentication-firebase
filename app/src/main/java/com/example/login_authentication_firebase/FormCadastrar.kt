e6ed4ba9096977fbeaa11f411e648b1ad9a1fb6c
lucianolopez85 <lopezz.lucio85@gmail.com>
2021-06-17 15:45:08 -0300

    form_cadastro

--- app/src/main/java/com/example/login_authentication_firebase/FormCadastrar.kt
+++ app/src/main/java/com/example/login_authentication_firebase/FormCadastrar.kt
@@ -3,9 +3,9 @@ package com.example.login_authentication_firebase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class FormCadastrar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_form_cadastrar)
     }
 }
